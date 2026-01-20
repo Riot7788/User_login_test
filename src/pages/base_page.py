51b2e8f8983e4b84c2597de9591823c2c1abec4a
Aleksandr <alex19950318@gmail.com>
2026-01-20 17:58:20 +0300

    Added tests and installed Allure

--- src/pages/base_page.py
+++ src/pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
@@ -7,16 +8,20 @@ class BasePage:
         self.driver = driver
         self.wait = WebDriverWait(driver, timeout)
 
+    @allure.step("Найти элемент {locator}")
     def find(self, locator):
         return self.wait.until(EC.visibility_of_element_located(locator))
 
+    @allure.step("Клик по элементу {locator}")
     def click(self, locator):
         self.find(locator).click()
 
+    @allure.step("Ввести текст '{text}' в поле {locator}")
     def type(self, locator, text):
         element = self.find(locator)
         element.clear()
         element.send_keys(text)
 
+    @allure.step("Получить текущий URL")
     def get_current_url(self):
         return self.driver.current_url
